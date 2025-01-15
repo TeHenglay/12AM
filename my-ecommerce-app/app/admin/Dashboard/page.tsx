ffa1fbe18f1168b8be9d3c1e9885482b4dc16439
12AM <nexgen1510@gmail.com>
2025-01-15 10:14:44 +0700

    11

---
+++ my-ecommerce-app/app/admin/Dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminDashboard(){
+    return (
+        <div>
+            <h1 className="font-custom text-3xl font-bold mb-4">Admin Dashboard</h1>
+            <p className="font-custom text-lg">Welcome to admin Dashboard</p>
+        </div>
+    )
+}
